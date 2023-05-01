67367b819b43fc0f8bc7c2fd5cb930723bceb8c4
Shiyuan Huang <shiyuancn@live.cn>
2023-04-30 20:29:46 -0400

    Implement simple random walk with stalling

--- main.py
+++ main.py
@@ -43,6 +43,9 @@ if dataset == "BarabasiAlbert":
     elif args.sampler == "rcmh":
         loader = graph_sampler.RejectionControlMetropolisHastingsSampler(
             data, batch_size=47, budget=2, alpha=0.25)
+    elif args.sampler == "srws":
+        loader = graph_sampler.SimpleRandomWalkWithStallingSampler(
+            data, batch_size=47, budget=2)
 else:
     if args.sampler == "srw":
         loader = GraphSAINTRandomWalkSampler(
@@ -57,6 +60,9 @@ else:
     elif args.sampler == "rcmh":
         loader = graph_sampler.RejectionControlMetropolisHastingsSampler(
             data, batch_size=args.batch_size, budget=4, alpha=0.25)
+    elif args.sampler == "srws":
+        loader = graph_sampler.SimpleRandomWalkWithStallingSampler(
+            data, batch_size=args.batch_size, budget=4)
 
 model = models.GNNNetwork(dataset.num_node_features, hidden_channels=256,
                           out_channels=dataset.num_classes).to(device)
