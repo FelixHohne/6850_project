bacf7acceeab6c6e291174721b48b130c06e3d21
Felix Hohne <felix.m.hohne@gmail.com>
2023-05-01 21:33:28 -0400

    Add grid search codes

--- main.py
+++ main.py
@@ -7,6 +7,7 @@ import dataset
 import models
 import graph_sampler
 import pandas as pd
+import statistics
 import matplotlib.pyplot as plt
 
 
@@ -15,7 +16,10 @@ device = torch.device('cpu')
 parser = argparse.ArgumentParser("Large Scale Graph Learning Codes")
 parser.add_argument('--dataset', type=str, default="ErdosRenyi")
 parser.add_argument("--sampler", type=str, default="srw")
-parser.add_argument("--batch_size", type=int, default=500)
+parser.add_argument("--batch_size", type=int, default = 500)
+parser.add_argument("--per_epoch_plot", action='store_true')
+parser.add_argument("--num_epochs", type = int, default = 100)
+parser.add_argument("--num_runs", type = int, default = 50)
 
 args = parser.parse_args()
 print(args)
@@ -67,7 +71,7 @@ def train():
     model.train()
     total_loss = total_examples = 0
     for data in loader:
-        print(data)
+        # print(data)
         data = data.to(device)
         optimizer.zero_grad()
         out = model(data.x, data.edge_index)
@@ -92,28 +96,65 @@ def test():
     return accs
 
 
-overall_accs = []
-for epoch in range(1, 100):
-    loss = train()
-    accs = test()
-    epoch_dic = {
-        "epoch" : epoch, 
-        "loss": loss, 
-        "train": accs[0], 
-        "test": accs[-1]
-    }
-    overall_accs.append(epoch_dic)
+test_accs = []
+for i in range(args.num_runs):
+    cur_valid_acc = 0 
+    test_acc = 0
+    for epoch in range(1, args.num_epochs):
+        loss = train()
+        accs = test()
+        
+    
 
-    if args.dataset == "EllipticBitcoinDataset":
-        print(
-            f'Epoch: {epoch:02d}, Loss: {loss:.4f}, train acc: {accs[0]:04f}, test acc: {accs[1]:04f}')
-    else:
-        print(f'Epoch: {epoch:02d}, Loss: {loss:.4f}, train acc: {accs[0]:04f}, valid acc: {accs[1]:04f}, test acc: {accs[2]:04f}' )
+        if args.dataset == "EllipticBitcoinDataset":
+            epoch_dic = {
+            "epoch" : epoch, 
+            "loss": loss, 
+            "train": accs[0], 
+            "test": accs[-1]
+        }
+            test_acc = epoch_dic["test"]
+        else:
+            epoch_dic = {
+            "epoch" : epoch, 
+            "loss": loss, 
+            "train": accs[0], 
+            "valid": accs[1], 
+            "test": accs[-1]
+            }
+            if epoch_dic["valid"] > cur_valid_acc:
+                cur_valid_acc = epoch_dic["valid"]
+                test_acc = epoch_dic["test"]
+    test_accs.append(test_acc)
+
+print(f"{args.dataset}-{args.sampler}-{args.batch_size}: test accuracy: {statistics.mean(test_accs)}, std: {statistics.stdev(test_accs)}")
+
+if args.per_epoch_plot:
+    overall_accs = []
+    for epoch in range(1, 1):
+        loss = train()
+        accs = test()
+
+        epoch_dic = {
+            "epoch" : epoch, 
+            "loss": loss, 
+            "train": accs[0], 
+            "test": accs[-1]
+        }
+        overall_accs.append(epoch_dic)
+
+        if args.dataset == "EllipticBitcoinDataset":
+            print(
+                f'Epoch: {epoch:02d}, Loss: {loss:.4f}, train acc: {accs[0]:04f}, test acc: {accs[1]:04f}')
+        else:
+            print(f'Epoch: {epoch:02d}, Loss: {loss:.4f}, train acc: {accs[0]:04f}, valid acc: {accs[1]:04f}, test acc: {accs[2]:04f}' )
 
 
-results_df = pd.DataFrame(
-    overall_accs
-)
+    results_df = pd.DataFrame(
+        overall_accs
+    )
 
-results_df.to_csv(f"{args.dataset}_{args.sampler}_{args.batch_size}.csv")
+    results_df.to_csv(f"{args.dataset}_{args.sampler}_{args.batch_size}.csv")
+
+
 
